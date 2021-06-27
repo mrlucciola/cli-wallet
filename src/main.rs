3abe44d298e5cb046b741412abe8910e9cade1c0
matthias <mrlucciola@protonmail.com>
2021-06-27 10:04:11 -0400

    send transaction complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use anyhow::Result;
-
+use web3::types::Address;
 pub mod wallet_lib;
-fn main() -> Result<()> {
-    let keypair = wallet_lib::create_keypair();
-    println!("keypair: {:?}", keypair);
+use crate::wallet_lib::{create_txn_obj, sign_and_send};
+use std::str::FromStr;
+use tokio;
+
+const URL: &str = "https://eth-ropsten.alchemyapi.io/v2/xxxxxxxxx";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let keypair = wallet_lib::create_keypair()?;
+    println!("\nkeypair: {:?}", keypair);
+    
+    let web3 = wallet_lib::establish_web3_connection(URL)?;
+    println!("\nweb3: {:?}", web3);
+    
+    let to_addr = Address::from_str("0xaaa5b9e6c589642f98a1cda99b9d024b8407285a")?;
+    println!("\nto_addr: {:?}", to_addr);
+    
+    let tx_obj = create_txn_obj(to_addr, 7)?;
+    println!("\ntx_obj: {:?}", tx_obj);
+
+    let result = sign_and_send(web3.clone(), tx_obj, keypair.0).await?;
+    
+    println!("\nresult: {}", result);
 
     Ok(())
 }
--- src/wallet_lib.rs
+++ src/wallet_lib.rs
@@ -1,8 +1,29 @@
 use anyhow::Result;
 use secp256k1::{PublicKey, Secp256k1, SecretKey, rand::{SeedableRng, rngs::StdRng}};
+use web3::{Web3, transports::Http, types::{H160, H256, TransactionParameters, U256}};
 
 pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
     let secp = Secp256k1::new();
     let mut rng = StdRng::seed_from_u64(6);
     Ok(secp.generate_keypair(&mut rng))
+}
+
+pub fn establish_web3_connection(url: &str) -> Result<Web3<Http>> {
+    let transport = Http::new(url)?;
+    Ok(web3::Web3::new(transport))
+}
+
+pub fn create_txn_obj(to: H160, value: usize) -> Result<TransactionParameters> {
+    Ok(TransactionParameters{
+        to: Some(to),
+        value: U256::exp10(value),
+        nonce: Some(U256([0,0,0,0])),
+        chain_id: Some(1),
+        ..Default::default()
+    })
+}
+
+pub async fn sign_and_send(web3: Web3<Http>, tx_object: TransactionParameters, seckey: SecretKey) -> Result<H256> {
+    let signed = web3.accounts().sign_transaction(tx_object, &seckey).await?;
+    Ok(web3.eth().send_raw_transaction(signed.raw_transaction).await?)
 }
